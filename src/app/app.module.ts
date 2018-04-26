a9345511101b5e1d55729cad13b7419334bf7717
Brian Williams <brian@development.mindplay>
2018-04-26 15:34:29 -0700

    Added routing and some basic pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,32 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home.component';
+import { PostComponent } from './post.component';
+import { PageNotFoundComponent } from './404.component';
 
+import { RouterModule, Routes } from '@angular/router';
+import { environment } from '../environments/environment';
+
+const appRoutes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'post/:id',      component: PostComponent },
+  { path: '',
+    redirectTo: '/home',
+    pathMatch: 'full'
+  },
+  { path: '**', component: PageNotFoundComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, HomeComponent, PostComponent, PageNotFoundComponent
   ],
   imports: [
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: environment.production == false } 
+    ),
     BrowserModule
   ],
   providers: [],
